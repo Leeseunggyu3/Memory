18a3e07571b4bdb9a39561a839376baf7a51e932
멍멍민 <pr620718@cu.ac.kr>
2025-05-24 01:17:15 +0900

    🚀 feat: 카드 정보 수신
    - Server에서 생성한 카드 정보를 받고 카드 UI를 갱신

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,16 @@ using System.Net.Sockets;
 using System;
 using System.Runtime.InteropServices;
 using System.Text;
+using static Define;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct Card // 카드 전송을 위해 만듦
 {
     public int id; // 카드 id 번호
+    [MarshalAs(UnmanagedType.I1)]
     public byte isFlip; // 카드가 앞면인가? (뒤집혔는가)
+    [MarshalAs(UnmanagedType.I1)]
     public byte isLock; // 맞춘 카드인가
-    //[MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
 };
 
 public class GameManager : MonoBehaviour
@@ -62,10 +64,11 @@ public class GameManager : MonoBehaviour
     NetworkStream Stream;
     #endregion
 
+    Card[] Cards = new Card[CARD_COUNT];
+
     void Start()
     {
         TryConnect();
-        // TODO: 접속한 클라이언트가 2명일 때까지 대기
 
         AudioSource[] sources = GetComponents<AudioSource>();
         bgmSource = sources[0];
@@ -82,6 +85,8 @@ public class GameManager : MonoBehaviour
         if (startSound != null)
             audioSource.PlayOneShot(startSound);
 
+        WaitForGameStart();
+
         GenerateCards();
         StartCoroutine(DistributeCardsSmoothly());
         UpdateTurnUI();
@@ -91,46 +96,17 @@ public class GameManager : MonoBehaviour
     {
         try
         {
-            int bytesRead = 0;
-            int cardSize = Marshal.SizeOf<Card>(); // 구조체 크기 계산
-            byte[] buffer = new byte[cardSize * CARD_COUNT];
-            Card[] cards = new Card[CARD_COUNT];
-
             Client = new TcpClient();
             Client.Connect(IP, PORT);
             Stream = Client.GetStream();
+            
             Debug.Log("서버에 연결되었습니다.");
 
-            // 서버로부터 메시지 수신
-            {
-                byte[] buf = new byte[255];
-                int read = Stream.Read(buf, 0, buf.Length);
-
-                string message = Encoding.UTF8.GetString(buf, 0, read);
-                Debug.Log("서버로부터 수신: " + message);
-            }
-
-            while (bytesRead < buffer.Length)
-            {
-                int read = Stream.Read(buffer, bytesRead, buffer.Length - bytesRead);
-                if (read == 0)
-                {
-                    Debug.Log("서버 연결 끊겼는데요.");
-                    break;
-                }
-                bytesRead += read;
-            }
-
-            for (int i = 0; i < CARD_COUNT; i++)
-            {
-                byte[] slice = new byte[cardSize]; // 받은 바이트
-                Array.Copy(buffer, i * cardSize, slice, 0, cardSize); // 받은 byte 잘라서 각 배열 원소에 저장
-
-                GCHandle handle = GCHandle.Alloc(slice, GCHandleType.Pinned); // 가비지 컬렉션 호출 막음
-                cards[i] = Marshal.PtrToStructure<Card>(handle.AddrOfPinnedObject()); // 받은 byte 배열을 Card 구조체 형태로 변환
-                Debug.Log($"현재 생성된 카드 ID : {cards[i].id}"); // 로그 출력
-                handle.Free();
-            }
+            byte[] buffer = new byte[255];
+            int read = Stream.Read(buffer, 0, buffer.Length);
+            
+            string message = Encoding.UTF8.GetString(buffer, 0, read);
+            Debug.Log("서버로부터 수신: " + message);
         }
         catch (Exception e)
         {
@@ -138,24 +114,85 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void WaitForGameStart()
+    {
+        while (true)
+        {
+            byte[] buffer = new byte[1];
+            int read = Stream.Read(buffer, 0, buffer.Length);
+
+            if (read <= 0)
+            {
+                Debug.LogWarning("서버 연결 끊김 또는 오류");
+                break;
+            }
+
+            char message = (char)buffer[0];
+            Debug.Log("서버로부터 수신: " + message);
+
+            if (message == START_GAME)
+                break;
+        }
+    }
+
     void GenerateCards()
     {
-        // TODO: 서버에서 카드 생성해서 클라이언트에게 보내기
-        int[] ids = new int[24];
-        for (int i = 0; i < 12; i++) { ids[i * 2] = i; ids[i * 2 + 1] = i; }
-        Shuffle(ids);
+        //int[] ids = new int[24];
+        //for (int i = 0; i < 12; i++) { ids[i * 2] = i; ids[i * 2 + 1] = i; }
+        //Shuffle(ids);
 
-        for (int i = 0; i < ids.Length; i++)
+        //for (int i = 0; i < ids.Length; i++)
+        //{
+        //    GameObject cardObj = Instantiate(cardPrefab, cardParent);
+        //    RectTransform rect = cardObj.GetComponent<RectTransform>();
+        //    rect.anchoredPosition = Vector2.zero; // 중앙에서 시작
+
+        //    CardUI card = cardObj.GetComponent<CardUI>();
+        //    card.Setup(ids[i], frontSprites[ids[i]], backSprite, this);
+
+        //    cardTargets[card] = rect.anchoredPosition; // 목표 위치는 GridLayoutGroup 정렬 이후 설정 예정
+        //    allCards.Add(card);
+        //}
+
+        int bytesRead = 0;
+        int cardSize = Marshal.SizeOf<Card>();
+        byte[] buffer = new byte[cardSize * CARD_COUNT];
+
+        while (bytesRead < buffer.Length)
         {
+            int read = Stream.Read(buffer, bytesRead, buffer.Length - bytesRead);
+            
+            if (read <= 0)
+            {
+                Debug.LogWarning("서버 연결 끊김 또는 오류");
+                break;
+            }
+
+            bytesRead += read;
+        }
+
+        for (int i = 0; i < CARD_COUNT; i++)
+        {
+            byte[] slice = new byte[cardSize];
+            Array.Copy(buffer, i * cardSize, slice, 0, cardSize); // 받은 byte 잘라서 각 배열 원소에 저장
+
+            GCHandle handle = GCHandle.Alloc(slice, GCHandleType.Pinned); // 가비지 컬렉션 호출 막음
+            Cards[i] = Marshal.PtrToStructure<Card>(handle.AddrOfPinnedObject()); // 받은 byte 배열을 Card 구조체 형태로 변환
+            handle.Free();
+
+            Debug.Log($"서버로부터 수신: card id {Cards[i].id}");
+
+            #region Card UI 생성
             GameObject cardObj = Instantiate(cardPrefab, cardParent);
             RectTransform rect = cardObj.GetComponent<RectTransform>();
             rect.anchoredPosition = Vector2.zero; // 중앙에서 시작
 
             CardUI card = cardObj.GetComponent<CardUI>();
-            card.Setup(ids[i], frontSprites[ids[i]], backSprite, this);
+            card.Setup(Cards[i].id, frontSprites[Cards[i].id], backSprite, this);
 
             cardTargets[card] = rect.anchoredPosition; // 목표 위치는 GridLayoutGroup 정렬 이후 설정 예정
             allCards.Add(card);
+            #endregion
         }
     }
 
@@ -191,14 +228,14 @@ public class GameManager : MonoBehaviour
         isCardGenerated = true;
     }
 
-    void Shuffle(int[] array)
-    {
-        for (int i = array.Length - 1; i > 0; i--)
-        {
-            int rand = UnityEngine.Random.Range(0, i + 1);
-            (array[i], array[rand]) = (array[rand], array[i]);
-        }
-    }
+    //void Shuffle(int[] array)
+    //{
+    //    for (int i = array.Length - 1; i > 0; i--)
+    //    {
+    //        int rand = UnityEngine.Random.Range(0, i + 1);
+    //        (array[i], array[rand]) = (array[rand], array[i]);
+    //    }
+    //}
 
     public void OnCardClicked(CardUI clickedCard)
     {
